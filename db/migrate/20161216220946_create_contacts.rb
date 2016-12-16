e6e7e7690af26f12a5f9bcf09cbe9abe5ceb9700
Jonathan Warshaw <jonathan.warshaw@gmail.com>
2016-12-16 16:15:37 -0600

    Constrain presence of all fields in contact table

--- db/migrate/20161216220946_create_contacts.rb
+++ db/migrate/20161216220946_create_contacts.rb
@@ -1,10 +1,10 @@
 class CreateContacts < ActiveRecord::Migration
   def change
     create_table :contacts do |t|
-      t.string :first_name
-      t.string :last_name
-      t.string :email
-      t.string :message
+      t.string :first_name, null: false
+      t.string :last_name,  null: false
+      t.string :email,      null: false
+      t.string :message,    null: false
 
       t.timestamps null: false
     end
