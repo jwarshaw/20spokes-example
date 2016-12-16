8d4a52d3e20eb6c73af41192a820082f912c7688
Jonathan Warshaw <jonathan.warshaw@gmail.com>
2016-12-16 16:50:21 -0600

    Write contact model tests for writeability

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 require 'spec_helper'
 
 RSpec.describe Contact, type: :model do
-  describe "reader attributes" do
+  describe "readability of attributes" do
     let (:contact) { Contact.create(first_name: "Jonathan", last_name: "Warshaw", email: "jonathan.warshaw@gmail.com", message: "and now for something completely different...") }
 
     it "has a readable first name" do
@@ -19,6 +19,30 @@ RSpec.describe Contact, type: :model do
 
     it "has a readable message" do
       expect(contact.message).to eq "and now for something completely different..."
+    end
+  end
+   
+  describe "writeability of attributes" do
+    let (:contact) { Contact.create(first_name: "Jonathan", last_name: "Warshaw", email: "jonathan.warshaw@gmail.com", message: "and now for something completely different...") }
+
+    it "has a writeable first name" do
+      contact.first_name = "Peter"
+      expect(contact.first_name).to eq "Peter"
+    end
+
+    it "has a writeable last name" do
+      contact.last_name = "Pan"
+      expect(contact.last_name).to eq "Pan"
+    end
+
+    it "has a writeable email" do
+      contact.email = "peter.pan@gmail.com"
+      expect(contact.email).to eq "peter.pan@gmail.com"
+    end
+
+    it "has a writeable message" do
+      contact.message = "To die will be an awfully big adventure."
+      expect(contact.message).to eq "To die will be an awfully big adventure."
     end
   end
 end
