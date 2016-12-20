51b6bc943cf99f5f9ce487dafd4c571fa0def379
Jonathan Warshaw <jonathan.warshaw@gmail.com>
2016-12-20 17:39:56 -0600

    Create and set initial state of form inputs

--- app/assets/javascripts/components/contact.js.jsx
+++ app/assets/javascripts/components/contact.js.jsx
@@ -1,12 +1,16 @@
 var ContactForm = React.createClass({ 
   displayName: "ContactForm",
 
+  getInitialState: function() {
+    return {firstName: '', lastName: '', email: '', message: ''};
+  },
+
   handleSubmit: function(event){
     event.preventDefault();
     console.warn('submit has been triggered');
 
     //Submit to server and render success message to user
-    
+
   },
 
   render: function(){
@@ -14,6 +18,22 @@ var ContactForm = React.createClass({
       <div className="container">
         <div className="col-sm-12">
           <form onSubmit={this.handleSubmit}>
+          <label>
+            First name:
+              <input type="text" name="first-name" />
+            </label>
+            <label>
+              Last name:
+              <input type="text" name="last-name" />
+            </label>
+            <label>
+              email:
+              <input type="email" name="email" />
+            </label>
+            <label>
+              Message:
+              <textarea value={this.state.message} onChange={this.handleChange} />
+            </label>
             <div className="actions">
               <button className="btn btn-primary">Submit</button>
             </div>
